2d1a1d02a2c0d97d7891fa7e42efc72bc7af3ae3
beingintheEra <bloomarosa2@gmail.com>
2026-08-09 12:43:59 +0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -224,13 +224,13 @@ function makeButton(label, onClick) {
   } else if (lower === "planned development") {
     btn.classList.add("btn-planneddevelopment");
     btn.innerHTML = `
-      <img class="btn-icon-img" src="./assets/buttons/planned development.svg" alt="">
+      <img class="btn-icon-img" src="./assets/buttons/business.svg" alt="">
       ${label}
     `;
   } else if (lower === "organic development") {
     btn.classList.add("btn-organicdevelopment");
     btn.innerHTML = `
-      <img class="btn-icon-img" src="./assets/buttons/organic development.svg" alt="">
+      <img class="btn-icon-img" src="./assets/buttons/home.svg" alt="">
       ${label}
     `;
     
