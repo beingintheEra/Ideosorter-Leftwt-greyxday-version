b90ddbe2f4e77056ab652a3536cb1654a00f2bca
beingintheEra <bloomarosa2@gmail.com>
2026-08-09 22:57:06 +0300

    Refactor button creation logic for better clarity

--- script.js
+++ script.js
@@ -32,47 +32,6 @@ function clearApp() {
 function makeButton(label, onClick) {
   const btn = document.createElement("button");
   const lower = label.toLowerCase();
-  const PROPP_BUTTON_CLASSES = {
-    "institutions": "btn-institutions",
-    "identity": "btn-identity",
-    "values": "btn-values",
-    "national": "btn-national",
-    "regional": "btn-regional",
-
-    "fair competition": "btn-faircompetition",
-    "fair outcomes": "btn-fairoutcomes",
-
-    "charisma": "btn-charisma",
-    "armed forces": "btn-armedforces",
-    "connections": "btn-connections",
-
-    "politics": "btn-politics",
-    "guerrilla": "btn-guerrilla",
-    "terrorism": "btn-terrorism",
-
-    "state": "btn-state",
-    "labor": "btn-labor",
-    "business": "btn-business",
-
-    "clerics": "btn-clerics",
-    "warriors": "btn-warriors",
-
-    "judgment": "btn-judgment",
-    "commandment": "btn-commandment",
-    "management": "btn-management",
-
-    "spiritual": "btn-spiritual",
-    "temporal": "btn-temporal",
-
-    "birthright": "btn-birthright",
-    "share holding": "btn-shareholding",
-    "land ownership": "btn-landownership",
-    "military honors": "btn-militaryhonors"
-  };
-
-  if (PROPP_BUTTON_CLASSES[lower]) {
-    btn.classList.add(PROPP_BUTTON_CLASSES[lower]);
-  }
 
   if (lower === "yes") {
     btn.classList.add("btn-yes");
@@ -274,76 +233,161 @@ function makeButton(label, onClick) {
       <img class="btn-icon-img" src="./assets/buttons/home.svg" alt="">
       ${label}
     `;
+   } else if (lower === "institutions") {
+    btn.classList.add("btn-institutions");
+    btn.innerHTML = `
+      <img class="btn-icon-img" src="./assets/buttons/institutions.svg" alt="">
+      ${label}
+    `;
+  } else if (lower === "identity") {
+    btn.classList.add("btn-identity");
+    btn.innerHTML = `
+      <img class="btn-icon-img" src="./assets/buttons/identity.svg" alt="">
+      ${label}
+    `;
+  } else if (lower === "values") {
+    btn.classList.add("btn-values");
+    btn.innerHTML = `
+      <img class="btn-icon-img" src="./assets/buttons/values.svg" alt="">
+      ${label}
+    `;
+  } else if (lower === "national") {
+    btn.classList.add("btn-national");
+    btn.innerHTML = `
+      <img class="btn-icon-img" src="./assets/buttons/national.svg" alt="">
+      ${label}
+    `;
+  } else if (lower === "regional") {
+    btn.classList.add("btn-regional");
+    btn.innerHTML = `
+      <img class="btn-icon-img" src="./assets/buttons/regional.svg" alt="">
+      ${label}
+    `;
+  } else if (lower === "fair competition") {
+    btn.classList.add("btn-faircompetition");
+    btn.innerHTML = `
+      <img class="btn-icon-img" src="./assets/buttons/fair competition.svg" alt="">
+      ${label}
+    `;
+  } else if (lower === "fair outcomes") {
+    btn.classList.add("btn-fairoutcomes");
+    btn.innerHTML = `
+      <img class="btn-icon-img" src="./assets/buttons/fair outcomes.svg" alt="">
+      ${label}
+    `;
   } else if (lower === "charisma") {
+    btn.classList.add("btn-charisma");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/charisma.svg" alt="">
       ${label}
     `;
   } else if (lower === "armed forces") {
+    btn.classList.add("btn-armedforces");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/armed forces.svg" alt="">
       ${label}
     `;
   } else if (lower === "connections") {
+    btn.classList.add("btn-connections");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/connections.svg" alt="">
       ${label}
     `;
+  } else if (lower === "politics") {
+    btn.classList.add("btn-politics");
+    btn.innerHTML = `
+      <img class="btn-icon-img" src="./assets/buttons/politics.svg" alt="">
+      ${label}
+    `;
+  } else if (lower === "guerrilla") {
+    btn.classList.add("btn-guerrilla");
+    btn.innerHTML = `
+      <img class="btn-icon-img" src="./assets/buttons/guerrilla.svg" alt="">
+      ${label}
+    `;
+  } else if (lower === "terrorism") {
+    btn.classList.add("btn-terrorism");
+    btn.innerHTML = `
+      <img class="btn-icon-img" src="./assets/buttons/terrorism.svg" alt="">
+      ${label}
+    `;
   } else if (lower === "clerics") {
+    btn.classList.add("btn-clerics");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/clerics.svg" alt="">
       ${label}
     `;
   } else if (lower === "warriors") {
+    btn.classList.add("btn-warriors");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/warriors.svg" alt="">
       ${label}
     `;
+
   } else if (lower === "judgment") {
+    btn.classList.add("btn-judgment");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/judgment.svg" alt="">
       ${label}
     `;
   } else if (lower === "commandment") {
+    btn.classList.add("btn-commandment");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/commandment.svg" alt="">
       ${label}
     `;
   } else if (lower === "management") {
+    btn.classList.add("btn-management");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/management.svg" alt="">
       ${label}
     `;
+
   } else if (lower === "spiritual") {
+    btn.classList.add("btn-spiritual");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/spiritual.svg" alt="">
       ${label}
     `;
   } else if (lower === "temporal") {
+    btn.classList.add("btn-temporal");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/temporal.svg" alt="">
       ${label}
     `;
   } else if (lower === "birthright") {
+    btn.classList.add("btn-birthright");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/birthright.svg" alt="">
       ${label}
     `;
+  } else if (lower === "military honors") {
+    btn.classList.add("btn-militaryhonors");
+    btn.innerHTML = `
+      <img class="btn-icon-img" src="./assets/buttons/military honors.svg" alt="">
+      ${label}
+    `;
   } else if (lower === "state") {
+    btn.classList.add("btn-state");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/state.svg" alt="">
       ${label}
     `;
   } else if (lower === "labor") {
+    btn.classList.add("btn-labor");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/labor.svg" alt="">
       ${label}
     `;
   } else if (lower === "business") {
+    btn.classList.add("btn-business");
     btn.innerHTML = `
       <img class="btn-icon-img" src="./assets/buttons/business.svg" alt="">
       ${label}
     `;
+  } else {
+    btn.textContent = label;
+  }
   } else {
     btn.textContent = label;
   }
